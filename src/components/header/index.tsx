ac833e7efa45342417503b6a424516221750040c
Caner Ören <caneroren7@gmail.com>
2023-02-17 15:48:30 +0300

    header ui

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image"
+import Link from "next/link"
 import styles from "./style.module.scss"
 
 export default function Header() {
@@ -5,7 +7,31 @@ export default function Header() {
     <header className={styles.header}>
       <div className="container">
         <div className="wrapper">
-          Header
+          <div className={styles.block}>
+            <Link href="/" className={styles.logo}>
+              <Image
+                src="/logos/caneroren-logo@2x.png"
+                alt="Caner Ören"
+                fill={true}
+              />
+            </Link>
+            <nav className={styles.menu}>
+              <ul>
+                <li>
+                  <Link href="/">Anasayfa</Link>
+                </li>
+                <li>
+                  <Link href="/">Projeler</Link>
+                </li>
+                <li>
+                  <Link href="/">Yazılar</Link>
+                </li>
+                <li>
+                  <Link href="/">Kaydedilenler</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
         </div>
       </div>
     </header>
