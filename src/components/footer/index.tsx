70bb884221bf2dacd4c27b2fb5736dc2da3b65a6
Caner Ören <caneroren7@gmail.com>
2023-02-17 16:16:24 +0300

    footer ui

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,10 +1,32 @@
+import Link from "next/link"
 import styles from "./style.module.scss"
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
       <div className="container">
-        <div className="wrapper">Footer</div>
+        <div className={styles.block}>
+          <h4 className={styles.title}>Bağlantılar</h4>
+          <nav className={styles.menu}>
+            <ul>
+              <li>
+                <Link href="/" target="_blank">Instagram</Link>
+              </li>
+              <li>
+                <Link href="/" target="_blank">Twitter</Link>
+              </li>
+              <li>
+                <Link href="/" target="_blank">Github</Link>
+              </li>
+              <li>
+                <Link href="/" target="_blank">Youtube</Link>
+              </li>
+              <li>
+                <Link href="/" target="_blank">E-Mail</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </div>
     </footer>
   )
